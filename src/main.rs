a8e00b29b51b3dd603ec323787f17253174586db
Oghenemarho Orukele <orukele.dev@gmail.com>
2026-01-04 21:16:08 +0100

    confused as fok

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use log::info;
+use std::fs;
+use std::fs::File;
+
 fn main() {
-    println!("Hello, world!");
+    env_logger::init();
+
+    let file_path = "./t8.shakespeare.txt";
+    let buffer = fs::read(file_path).expect("unable to read file into bytes");
+
+    info!("Size of {} is {} bytes", &file_path, &buffer.len());
+
+    // convert buffer to string view
+    let buffer = String::from_utf8(buffer).expect("unable to convert buffer to string");
+    let content: &str = &buffer;
+
+    for token in content.split_whitespace() {
+        info!("{:?}", token);
+    }
 }
